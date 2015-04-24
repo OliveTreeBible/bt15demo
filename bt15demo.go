42a5078241c4afc6b1058cb028b9684f5cb7a597
David Trotz <davidtrotz@olivetree.com>
2015-04-24 16:21:11 -0700

    Refactored to match the presentation steps
    Not horribly efficient but follows the steps in my presentation.

--- bt15demo.go
+++ bt15demo.go
@@ -18,27 +18,47 @@ const (
 	Ideo
 )
 
-func ExampleScrape(url string) {
-  doc, err := goquery.NewDocument(url)
+type ChunkInfo struct {
+  totalWords int
+  chunk string
+}
+
+func excuteDemoForURL(documentURL string) {
+  doc := fetchTheDocument(documentURL)
   
+  chunks := chunkTheText(doc)
+  hashes := hashTheChunks(chunks)
+  success := indexTheHashes(hashes)
+  if success {
+	fmt.Println("Done!")
+  }
+}
+
+func fetchTheDocument(documentURL string) (*goquery.Document) {
+  doc, err := goquery.NewDocument(documentURL)
+  
+  if err != nil {
+    log.Fatal(err)
+  }
+  return doc
+}
+
+func prepTheDatabase() {
   c, _ := sqlite3.Open("sqlite.db")
   c.Exec("DROP TABLE IF EXISTS block_index")
   c.Exec("CREATE TABLE block_index (block_id INTEGER PRIMARY KEY, block_hash VARCHAR(255) NOT NULL, total_words INTEGER NOT NULL)")
   c.Exec("CREATE INDEX block_hash_idx ON block_index (block_hash)")
   // go use(c)
-  
-  if err != nil {
-    log.Fatal(err)
-  }
+}
 
+func chunkTheText(doc *goquery.Document) ([]ChunkInfo) {
+  var chunks []ChunkInfo
   breakAccountedFor := false
-  normalizedString := ""
-  blockWordCount := 0
+  chunk := ""
+  totalWords := 0
   
   doc.Find("p").Each(func(i int, s *goquery.Selection) {
     text := s.Text()
-	blockWordCount = 0
-	normalizedString = ""
 	
     segmenter := segment.NewWordSegmenterDirect([]byte(text))
     for segmenter.Segment() {
@@ -47,12 +67,12 @@ func ExampleScrape(url string) {
 		
 		
 		if tokenType != None   {
-				normalizedString += segmenter.Text()
+				chunk += segmenter.Text()
 				breakAccountedFor = false
-				blockWordCount++
+				totalWords++
 		} else if tokenType == None {
 				if !breakAccountedFor {
-						normalizedString += " "
+						chunk += " "
 						breakAccountedFor = true
 				}
 		} else {
@@ -64,14 +84,34 @@ func ExampleScrape(url string) {
         log.Fatal(err)
     }
 	
-    hash := sha256.New()
-    hash.Write([]byte(normalizedString))
-    md := hash.Sum(nil)
-    mdStr := hex.EncodeToString(md)
-    fmt.Printf("\n%s\n====> %d words %s\n", normalizedString, blockWordCount, mdStr)
+	if totalWords >= 50 {
+	  chunks = append(chunks, ChunkInfo{totalWords, chunk})
+	  // Reset for the next round
+	  totalWords = 0
+	  chunk = ""
+	}
   })
+  return chunks
+}
+
+func hashTheChunks(chunks []ChunkInfo) ([]string) {
+  var hashes []string = make([]string, 0, len(chunks))
+  
+  for i, chunkInfo := range chunks {
+	hasher := sha256.New()
+    hasher.Write([]byte(chunkInfo.chunk))
+    hash := hasher.Sum(nil)
+    hashes = append(hashes, hex.EncodeToString(hash))
+	fmt.Printf("\n%s\n====> %d words %s\n", chunkInfo.chunk, chunkInfo.totalWords, hashes[i])
+  }
+  
+  return hashes
+}
+
+func indexTheHashes(hashes []string) (bool) {
+  return false
 }
 
 func main() {
-    ExampleScrape("https://www.gutenberg.org/files/39452/39452-h/39452-h.htm")
+    excuteDemoForURL("https://www.gutenberg.org/files/39452/39452-h/39452-h.htm")
 }
