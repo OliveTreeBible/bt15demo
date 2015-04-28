2e40bca19955602ddf41ed1cc2f92b03722622ef
David Trotz <davidtrotz@olivetree.com>
2015-04-28 13:56:47 -0700

    Some refactoring and implementation of the index step

--- bt15demo.go
+++ bt15demo.go
@@ -21,37 +21,32 @@ const (
 )
 
 type ChunkInfo struct {
+  positionIndex int
   totalWords int
   chunk string
+  hash string
 }
 
-func fetchTheDocument(documentURL string) (*goquery.Document) {
-  doc, err := goquery.NewDocument(documentURL)
-  
-  if err != nil {
-    log.Fatal(err)
-  }
-  return doc
+type DocumentInfo struct {
+  documentURL string
+  databaseFileName string
+  databaseHandle *sqlite3.Conn
+  htmlDocument *goquery.Document
+  chunks []ChunkInfo
 }
 
-func prepTheDatabase() {
-  c, _ := sqlite3.Open("sqlite.db")
-  c.Exec("DROP TABLE IF EXISTS block_index")
-  c.Exec("CREATE TABLE block_index (block_id INTEGER PRIMARY KEY, block_hash VARCHAR(255) NOT NULL, total_words INTEGER NOT NULL)")
-  c.Exec("CREATE INDEX block_hash_idx ON block_index (block_hash)")
-  // go use(c)
-}
-
-func chunkTheText(doc *goquery.Document) ([]ChunkInfo) {
-  var chunks []ChunkInfo
+func chunkTheText(docInfo *DocumentInfo) bool {
+  failed := false
   breakAccountedFor := false
   chunk := ""
   totalWords := 0
+  positionIndex := -1
   
-  doc.Find("p,pre,div,span,h1,h2,h3").Each(func(i int, s *goquery.Selection) {
+  docInfo.htmlDocument.Find("p,pre,div,span,h1,h2,h3").Each(func(i int, s *goquery.Selection) {
+	if !failed {
 	// A bit of a bug here as this does not handle tags embedded in the text at
 	// all so for instance if you have:
-	// 		'... some text<span>blah blah</span>more text...'
+	//		'... some text<span>blah blah</span>more text...'
 	// you will get '... some textblah blahmore text...' which most likely isn't
 	// what you want. This is an area that would need to be discussed in detail
 	// if this approach was to be standardized for sharng annotations or for
@@ -60,6 +55,7 @@ func chunkTheText(doc *goquery.Document) ([]ChunkInfo) {
 	if	len(chunk) > 0 && !strings.HasSuffix(chunk, " ") {
 	  chunk += " "
 	}
+	positionIndex = i//docInfo.htmlDocument.IndexOfSelection(s)
 	
     segmenter := segment.NewWordSegmenterDirect([]byte(text))
     for segmenter.Segment() {
@@ -82,56 +78,76 @@ func chunkTheText(doc *goquery.Document) ([]ChunkInfo) {
     }
 	
     if err := segmenter.Err(); err != nil {
-        log.Fatal(err)
-    }
-	
-	if totalWords >= 50 {
-	  chunks = append(chunks, ChunkInfo{totalWords, chunk})
+		log.Fatal(err)
+        failed = true
+    } else if totalWords >= 50 {
+	  docInfo.chunks = append(docInfo.chunks, ChunkInfo{positionIndex, totalWords, chunk, ""})
 	  // Reset for the next round
 	  breakAccountedFor = false
 	  totalWords = 0
 	  chunk = ""
 	}
+	}
   })
   
   // In case there is a straggler 
-  if len(chunk) > 0 && totalWords > 0 {
-	chunks = append(chunks, ChunkInfo{totalWords, chunk})
+  if !failed && len(chunk) > 0 && totalWords > 0 {
+	docInfo.chunks = append(docInfo.chunks, ChunkInfo{positionIndex, totalWords, chunk, ""})
   }
   
-  return chunks
+  return !failed
 }
 
-func hashTheChunks(chunks []ChunkInfo) ([]string) {
-  var hashes []string = make([]string, 0, len(chunks))
-  
-  for i, chunkInfo := range chunks {
+func hashTheChunks(docInfo *DocumentInfo) (bool){
+  for index := range docInfo.chunks {
 	hasher := sha256.New()
-    hasher.Write([]byte(chunkInfo.chunk))
+    hasher.Write([]byte(docInfo.chunks[index].chunk))
     hash := hasher.Sum(nil)
-    hashes = append(hashes, hex.EncodeToString(hash))
-	fmt.Printf("\n%s\n====> %d words %s\n", chunkInfo.chunk, chunkInfo.totalWords, hashes[i])
+    docInfo.chunks[index].hash = hex.EncodeToString(hash)
+	fmt.Printf("\n%s\n====> %d words %s\n", docInfo.chunks[index].chunk, docInfo.chunks[index].totalWords, docInfo.chunks[index].hash)
   }
-  
-  return hashes
+  return true
 }
 
-func indexTheHashes(hashes []string) (bool) {
-  // TODO:
-  return false
+func indexTheHashes(docInfo *DocumentInfo) (bool) {
+  fmt.Println("Chunks ", len(docInfo.chunks))
+  for _, chunk := range docInfo.chunks {
+	args := sqlite3.NamedArgs{"$block_hash": chunk.hash, "$total_words": chunk.totalWords, "$position_index":chunk.positionIndex}
+	docInfo.databaseHandle.Exec("INSERT INTO block_index (block_hash, total_words, position_index) VALUES($block_hash, $total_words, $position_index)", args)
+  }
+  return true
+}
+
+func prepDocInfo(docInfo *DocumentInfo) (error) {
+  var err error
+  flag.StringVar(&docInfo.documentURL, "document-url", "https://www.gutenberg.org/files/39452/39452-h/39452-h.htm", "The input document URL that you wish to process.")
+  flag.StringVar(&docInfo.databaseFileName, "database-filename", "btdemo15.sqlite", "The output filename for the datbase index.")
+  flag.Parse()
+  
+  docInfo.htmlDocument, err = goquery.NewDocument(docInfo.documentURL)
+  if err == nil {
+	docInfo.databaseHandle, err = sqlite3.Open(docInfo.databaseFileName)
+	if err == nil {
+	  docInfo.databaseHandle.Exec("DROP TABLE IF EXISTS block_index")
+	  docInfo.databaseHandle.Exec("CREATE TABLE block_index (block_id INTEGER PRIMARY KEY ASC AUTOINCREMENT, block_hash VARCHAR(255) NOT NULL, total_words INTEGER NOT NULL, position_index INTEGER DEFAULT -1)")
+	  docInfo.databaseHandle.Exec("CREATE INDEX block_hash_idx ON block_index (block_hash)")
+	}
+  }
+  
+  return err
 }
 
 func main() {
-  var documentURL string
-  flag.StringVar(&documentURL, "document-url", "https://www.gutenberg.org/files/39452/39452-h/39452-h.htm", "The document URL that you wish to process.")
-  flag.Parse()
+  docInfo := DocumentInfo{}
   
-  doc := fetchTheDocument(documentURL)
-  
-  chunks := chunkTheText(doc)
-  hashes := hashTheChunks(chunks)
-  success := indexTheHashes(hashes)
-  if success {
-	fmt.Println("Done!")
+  if err := prepDocInfo(&docInfo); err == nil {
+	if chunkTheText(&docInfo) &&
+	   hashTheChunks(&docInfo) &&
+	   indexTheHashes(&docInfo) {
+		fmt.Println("Success!")
+	}
+  } else {
+	log.Fatal(err)
   }
+  docInfo.databaseHandle.Close()
 }
